39e4710a8cbf7e2dda92cec31d6dff20d7edb43c
barkode <konstantinks@gmail.com>
2024-10-23 12:49:08 +0100

    Refactor import statement for views in urls.py
    Modified the import statement for views to use relative import to maintain consistency within the app module. Added a comma at the end of urlpatterns for cleaner diffs in future commits.

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from home import views
+from . import views
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     ]
