974614adc1ac0dfbad115d6eadbc8a5f2360d7dd
barkode <konstantinks@gmail.com>
2024-10-23 13:41:47 +0100

    Simplify URL configurations for home app
    Removed redundant namespace and simplified URL patterns for the home app. Adjusted the base template to reflect changes in URL naming. This enhances code readability and reduces potential points of failure.

--- app/urls.py
+++ app/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls', namespace='home')),
+    path('', include('home.urls')),
     ]
 
 if settings.DEBUG:
