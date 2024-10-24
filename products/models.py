34761c4bd61e81c505a4f68cdb6f4386a83e36e2
barkode <konstantinks@gmail.com>
2024-10-24 13:45:35 +0100

    Update product and category models
    Added verbose name options in Category meta class. Modified Product model to include default image, URL field, rating, and auto timestamp for updates. Enhanced string representation formats for better clarity.

--- products/models.py
+++ products/models.py
@@ -9,6 +9,10 @@ class Category(models.Model):
     image = models.ImageField(null=True, blank=True)
     slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
@@ -38,7 +42,7 @@ class Measurement(models.Model):
                                          null=True)
 
     def __str__(self):
-        return f"{self.name} ({self.abbreviation})"
+        return f"{self.name} | ({self.abbreviation})"
 
 
 class Product(models.Model):
@@ -47,12 +51,16 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default='noimage.png')
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     stock_quantity = models.PositiveIntegerField()
     added_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
     measurement = models.ForeignKey(Measurement, on_delete=models.SET_NULL, null=True)
     measurement_value = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -60,4 +68,4 @@ class Product(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name} ({self.measurement_value} {self.measurement.abbreviation})"
+        return f"{self.name} | ({self.measurement_value} | {self.measurement.abbreviation})"
