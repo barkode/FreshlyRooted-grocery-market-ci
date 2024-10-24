4817f74e4da771dfa97ed5abd2a5cbfa23571b74
barkode <konstantinks@gmail.com>
2024-10-24 13:45:45 +0100

    Add placeholder image 'noimage.png'
    Introduce a default placeholder image for scenarios where no image is provided. This helps maintain visual consistency and improves user experience by avoiding broken image links.

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
+
 from .models import Category, Measurement, MeasurementType, Product
+
+
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'friendly_name')
+
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ('sku', 'name', 'category', 'price', 'rating', 'image')
+
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation', 'measurement_type')
+
+
+class MeasurementTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_type')
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Measurement)
-admin.site.register(MeasurementType)
+admin.site.register(Measurement, MeasurementAdmin)
+admin.site.register(MeasurementType, MeasurementTypeAdmin)
