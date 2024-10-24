04131a10c8ac5bba96653232ca0353b519b5617c
barkode <konstantinks@gmail.com>
2024-10-24 16:06:47 +0100

    Add new models and fields to Product
    Introduced Currency, ProductMessage, and ShippingDay models. Updated Product model to include discount_percent, currency, message, and shipping_day fields. Adjusted Product admin and migrations accordingly.

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Measurement, MeasurementType, Product
+from .models import Category, Measurement, MeasurementType, Product, ProductMessage, ShippingDay, Currency
 
 
 # Register your models here.
@@ -10,12 +10,6 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'friendly_name',)
     ordering = ('name', 'friendly_name',)
 
-
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
-    list_display = ('sku', 'name', 'category', 'price', 'rating', 'image',)
-    ordering = ('sku', 'name', 'category', 'rating',)
-
 class MeasurementAdmin(admin.ModelAdmin):
     list_display = ('name', 'abbreviation', 'measurement_type',)
 
@@ -24,7 +18,17 @@ class MeasurementTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_type',)
 
 
-admin.site.register(Product, ProductAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('sku', 'name', 'category', 'price', 'rating', 'image',)
+    ordering = ('sku', 'name', 'category', 'rating',)
+
+
+
+admin.site.register(Currency)
+admin.site.register(ProductMessage)
+admin.site.register(ShippingDay)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Measurement, MeasurementAdmin)
 admin.site.register(MeasurementType, MeasurementTypeAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -45,22 +46,54 @@ class Measurement(models.Model):
         return f"{self.name} | ({self.abbreviation})"
 
 
+class ShippingDay(models.Model):
+    name = models.CharField(max_length=50)
+    days = models.PositiveIntegerField()  # e.g., the number of days for shipping
+
+    def __str__(self):
+        return self.name
+
+
+class ProductMessage(models.Model):
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=3)
+    symbol = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name} | {self.symbol}"
+
+
 class Product(models.Model):
+    added_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    sku = models.CharField(max_length=254, unique=True)
-    name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True, default='noimage.png')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sku = models.CharField(max_length=254, unique=True)
     stock_quantity = models.PositiveIntegerField()
-    added_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     measurement = models.ForeignKey(Measurement, on_delete=models.SET_NULL, null=True)
     measurement_value = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
+    shipping_day = models.ForeignKey(ShippingDay, on_delete=models.SET_NULL, null=True, blank=True)
+    message = models.ForeignKey(ProductMessage, on_delete=models.SET_NULL, null=True, blank=True)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, blank=True)
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-added_date"]
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
     def save(self, *args, **kwargs):
         if not self.slug:
