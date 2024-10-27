9bbae1a8791b8989f1953df5441448fe37e0e415
barkode <konstantinks@gmail.com>
2024-10-27 23:48:10 +0000

    Refactor SubCategory model inheritance
    Altered the SubCategory model to inherit from Category using a OneToOne relationship. This required removing redundant fields and adjusting related model properties. Also updated related_name attributes for category and sub_category in Product model.

--- products/models.py
+++ products/models.py
@@ -90,10 +90,10 @@ class Currency(models.Model):
 class Product(models.Model):
     added_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="category"
     )
     sub_category = models.ForeignKey(
-        SubCategory, on_delete=models.SET_NULL, null=True, blank=True
+        SubCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name="sub_category"
     )
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True, default="noimage.png")
