82bba30578e714f54aaa00287491da9c0916465c
barkode <konstantinks@gmail.com>
2024-10-27 17:09:15 +0000

    Change search URL to 'products_search'
    Updated the search forms in both mobile and desktop headers to point to the new 'products_search' endpoint. Also added the corresponding URL pattern in 'products/urls.py'. This ensures the search functionality works correctly across devices.

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('search/', views.all_products, name='products_search'),
     path('<int:product_id>', views.product_detail, name='product_detail'),
 
     ]
