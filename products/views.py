1b83c9bd77389eff600d936c4cf979ca5f042de0
barkode <konstantinks@gmail.com>
2024-10-28 01:46:37 +0000

    Add initial product model migrations and update templates
    Create initial migration file for products app including models for Product, Category, Currency, Measurement, MeasurementType, ProductMessage, and Shipping. Update product detail and product card templates to display categories dynamically. Add category-based URL filtering in header menu and modify product view to handle category names from URLs.

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 
-from .models import Product
+from .models import Category, Product
 
 
 # Create your views here.
@@ -12,12 +12,16 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
+            print('Categories before',categories)
             products = products.filter(category__name__in=categories)
+            print('Products cat',categories)
+            categories = Category.objects.filter(name__in=categories)
+            print('Categories after',categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -31,6 +35,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
         }
 
     return render(request, 'products/products.html', context)
