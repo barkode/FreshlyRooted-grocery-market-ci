c39092fc0ef365e602d97690e396e67d74f3edf3
barkode <konstantinks@gmail.com>
2024-10-30 18:02:22 +0000

    Add CRUD for products and update product templates
    Implemented forms, views, and templates to handle the addition, editing, and deletion of products. Refined the product display and search functionalities, ensuring seamless integration with categories and user permissions.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 
+from .forms import ProductForm
 from .models import Category, Product
 
 
@@ -11,9 +13,9 @@ def all_products(request):
     """ A view to show all products including sorting and search queries """
 
     products = Product.objects.all()
-    query = None
-    categories = None
-    sort = None
+    query = request.GET.get('q', None)
+    categories = request.GET.get('category', None)
+    sort = request.GET.get('sort', None)
 
     sort_options = {
         'default': 'name',
@@ -24,23 +26,26 @@ def all_products(request):
         }
 
     if request.GET:
-        if 'sort' in request.GET and sort_options.get(request.GET['sort']):
+        if sort:
             sortkey = request.GET.get('sort', 'default')
+            sort = sortkey
             products = products.order_by(sort_options[sortkey])
         else:
             products = products.order_by(sort_options['default'])
 
-        if 'category' in request.GET:
+
+        if categories:
             categories = request.GET['category'].split(',')
             products = products.filter(category__slug__in=categories)
             categories = Category.objects.filter(slug__in=categories)
+
+
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, 'You have not entered any search criteria!')
                 return redirect(reverse('products'))
-
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -66,3 +71,74 @@ def product_detail(request, product_id):
         }
 
     return render(request, 'products/product_detail.html', context)
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           ('Failed to add product. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+        }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           ('Failed to update product. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+        }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
