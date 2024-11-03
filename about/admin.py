65512836a1c6c26ecd7399474d499e834f314c17
barkode <konstantinks@gmail.com>
2024-11-03 00:52:00 +0000

    Add "About" and "CollaborateRequest" models with admin support
    Introduced "About" and "CollaborateRequest" models to store site information and collaboration requests. Registered these models with Django admin for content management. Added views, forms, and tests to handle displaying and submitting collaboration requests.

--- about/admin.py
+++ about/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import About, CollaborateRequest
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    """
+    Adds rich-text editing of content in admin
+    """
+
+    summernote_fields = ("content",)
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    """
+    Lists message and read fields for display in admin
+    """
+
+    list_display = (
+        "message",
+        "read",
+    )
