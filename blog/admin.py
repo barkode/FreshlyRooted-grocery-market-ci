08c3666000f2f56b6c7ef756dece9424776c014d
barkode <konstantinks@gmail.com>
2024-11-03 00:52:18 +0000

    Add Post, Comment models, admin configuration, and tests
    Added models for blog posts and comments, integrated admin functionalities for managing posts, and created views for displaying and managing comments. Implemented forms and basic tests to ensure proper functionality of comment creation and display.

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,23 @@
+from django.contrib import admin
+
+from .models import Comment, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    list_filter = (
+        "status",
+        "created_on",
+    )
+    prepopulated_fields = {"slug": ("title",)}
+
+
 # Register your models here.
+admin.site.register(Comment)
