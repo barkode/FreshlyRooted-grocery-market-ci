cfe759634ee15b0a764ac7c804841db39fb7b7be
barkode <konstantinks@gmail.com>
2024-11-07 09:59:58 +0000

    Remove obsolete static assets and templates
    Deleted numerous outdated static files, including images and fonts, and removed old email and form templates related to allauth and MFA. This cleanup improves project maintainability and reduces storage usage.

--- about/views.py
+++ about/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.contrib import messages
-from .models import About
+from django.shortcuts import render
+
 from .forms import CollaborateForm
+from .models import About
+
 
 # Create your views here.
 
