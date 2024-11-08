af5bef1ef3cd1101f8fde506d78da4fe027c9d92
barkode <konstantinks@gmail.com>
2024-11-08 05:23:28 +0000

    Add new logo and update UI colors and styles
    Introduced a new logo image and adjusted colors in various sections for consistency. Updated button and form field styles to rounded-1 and tweaked hero text positioning and background colors.

--- profiles/forms.py
+++ profiles/forms.py
@@ -37,6 +37,6 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs["placeholder"] = placeholder
             self.fields[field].widget.attrs["class"] = (
-                "border-black " "rounded-0 " "profile-form-input"
+                "border-black " "rounded-1 " "profile-form-input"
             )
             self.fields[field].label = False
