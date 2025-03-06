8872fe55a382a76d66b73edf79eb8a88950c6cf4
barkode <konstantinks@gmail.com>
2025-03-06 04:36:17 +0000

    Add Contact model and views; update index.html with new sections and improve carousel

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# Model representing a contact form submission
+class Contact(models.Model):
+    
+    email = models.EmailField(blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    message = models.TextField(blank=False)
+    date_posted = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name}, {self.email}"
+
+    class Meta:
+        verbose_name = "Contact Form Submission"
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
-app_name = "home"
 urlpatterns = [
-    path("", views.home, name="home"),
-]
+	path('', views.index, name='home'),
+	path('summernote/', include('django_summernote.urls')),
+	path("privacy_policy", views.privacy_policy, name="privacy_policy"),
+	path("returns", views.returns, name="returns"),
+	path("contact", views.contact, name="contact"),
+	]
